8fccd9f0cb03c7338d394e0a6de339d1837ec67d
Sebastian Metta <sebastianmetta@gmail.com>
2015-06-28 18:56:07 -0300

    Creando esqueleto del proyecto

---
+++ tp-fallas/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tp-fallas
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ tp-fallas/src/tp_fallas/core.clj
@@ -0,0 +1,7 @@
+(ns tp-fallas.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
