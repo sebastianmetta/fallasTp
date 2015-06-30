bfda86e6f029417ea329fe2110ab97fe12642920
sebastianmetta <sebastianmetta@gmail.com>
2015-06-29 23:31:19 -0300

    Codigo ejemplo para toma de datos del usuario

--- fallas_tp/src/fallas_tp/core.clj
+++ fallas_tp/src/fallas_tp/core.clj
@@ -1,7 +1,18 @@
 (ns fallas-tp.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(println "Bienvenido al sistema experto para el diagnóstico de causas ante fallas en equipos de refrigeración.")
+(println "Ingrese el tipo de equipo de refrigeración a diagnosticar:")
+(println "1. Refrigeracion Familiar")
+(println "2. Refrigeracion Comercial")
+(println "3. Aire acondicionado")
+(println ":")
+
+(def tipoDeEquipo (read-line))
+
+(if (= tipoDeEquipo "1")
+  (println "Seleccionado Refrigeración Famililar"))
+(if (= tipoDeEquipo "2")
+  (println "Seleccionado Refrigeracion Comercial"))
+(if (= tipoDeEquipo "3")
+  (println "Seleccionado Aire acondicionado"))
